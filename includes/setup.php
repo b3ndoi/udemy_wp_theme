42b13b5498dc01aa6b6da2609d3c93d727edaf66
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2018-08-09 22:27:03 +0200

    Added single.php

--- includes/setup.php
+++ includes/setup.php
@@ -3,6 +3,7 @@
 function ju_setup_theme()
 {
     // Adds menus to the theme
+ add_theme_support( 'post-thumbnails' );
  add_theme_support( 'menus' );
  register_nav_menu( 'primary', __( 'Primary Menu', 'undemy' ) );
 }
